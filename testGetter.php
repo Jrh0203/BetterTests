f9aa231f372a8d386751b67def88188a38841bb2
Jrh0203 <herjo345@gmail.com>
2017-09-22 01:44:36 -0500

    getter

--- testGetter.php
+++ testGetter.php
@@ -1,19 +1,16 @@
 <?php
-console("test");
 $dir = "";
-if($_GET["project"] == "2"){
-    $dir = "/u/gheith/public/cs439h_f17_p2/latest/";
-} else if($_GET["project"] == "3"){{
-    $dir = "/u/gheith/public/cs439h_f17_p3/latest/";
+if($_GET["project"] == "9"){
+    $dir = "/u/gheith/public/cs439h_f17_p2/";
 } else {
-    $dir = "/u/gheith/public/cs439h_f17_p4/latest/";
+    $dir = "/u/gheith/public/cs439h_f17_p3/";
 }
 $test = $_GET["test"];
 $files = scandir($dir);
 foreach ($files as $file){
     if($file != "." && $file != ".."){
         if($test == ""){
-            if(substr($file, -4) == ".hex"){
+            if(substr($file, -3) == ".cc"){
                 $data = substr($file, 0 , -4);
                 echo "<option value='" . $data . "'>" . $data . "</option>";
             }
