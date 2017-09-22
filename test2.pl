c50292742fe704555fb1b3a3ba087ead9248fad4
Jrh0203 <herjo345@gmail.com>
2017-09-22 01:39:39 -0500

    getter

--- test2.pl
+++ test2.pl
@@ -1,3 +1,20 @@
 #!/usr/bin/perl
-print "Content-type: text/html\n\n";
-print "Hello, World.";
+
+    use strict;
+    use warnings;
+
+    my $dir = '~gheith/public/cs439h_f17_p3/latest/';
+
+    opendir(DIR, $dir) or die $!;
+
+    while (my $file = readdir(DIR)) {
+
+        # Use a regular expression to ignore files beginning with a period
+        next if ($file =~ m/^\./);
+
+	print "$file\n";
+
+    }
+
+    closedir(DIR);
+    exit 0;
